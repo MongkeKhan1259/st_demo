13f5f7f217bf414c377455c71628303c40ac30e5
MongkeKhan1259 <166204445+MongkeKhan1259@users.noreply.github.com>
2024-08-22 09:18:40 -0400

    Update main.py

--- st_demo/main.py
+++ st_demo/main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.markdown('# my histogram')
+st.header('my histogram')
+st.write('some random text')
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
